4da9731f4563e62343935e8f0081d7f035b46d62
RodrigoAXCid <rodrigo.cid@qloudyx.pt>
2022-09-22 12:12:19 +0100

    continue

--- apps/animal/urls.py
+++ apps/animal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, AnimalCreateView
 
 app_name = 'animal'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('novo/', AnimalCreateView.as_view(), name='novo')
 ]
--- apps/animal/views.py
+++ apps/animal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Animal
 
 #Apresentação de página estática
 def home(request):
@@ -8,3 +9,8 @@ def home(request):
 
 def about(request):
     return render(request, 'animal/about.html')
+
+class AnimalCreateView(CreateView):
+    model = Animal
+    fields = '__all__'
+    success_url = reverse_lazy('animal:home')
