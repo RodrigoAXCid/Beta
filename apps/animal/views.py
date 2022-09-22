6149cf58211501420d456af5d0e0fa7701665ca0
RodrigoAXCid <rodrigo.cid@qloudyx.pt>
2022-09-22 10:57:17 +0100

    Gitignore

--- apps/animal/views.py
+++ apps/animal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Apresentação de página estática
+def home(request):
+    return render(request, 'animal/home.html')
+
+def about(request):
+    return render(request, 'animal/about.html')
