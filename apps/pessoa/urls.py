6ced2c2ec6fdaa85d442faf6da6cb6f1fc65e647
RodrigoAXCid <rodrigo.cid@qloudyx.pt>
2022-09-29 12:27:14 +0100

    DetailView

--- apps/pessoa/urls.py
+++ apps/pessoa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import contacts, PessoaCreateView, PessoaListView
+from .views import contacts, PessoaCreateView, PessoaListView, PessoaDetailView
 
 app_name = 'pessoa'
 
 urlpatterns = [
     path('', contacts, name='contacts'),
     path('novo/', PessoaCreateView.as_view(), name='novo'),
-    path('lista/', PessoaListView.as_view(), name='lista')
+    path('lista/', PessoaListView.as_view(), name='lista'),
+    path('detalhe/<int:pk>/', PessoaDetailView.as_view(), name='detalhe')
 ]
